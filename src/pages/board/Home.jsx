532c7f5d91a6788e1162116b9a3ea96b47c87dcd
Jin <jj1430413@gmail.com>
2026-06-02 10:55:57 +0900

    back연결

--- src/pages/board/Home.jsx
+++ src/pages/board/Home.jsx
@@ -52,7 +52,7 @@ const Home =()=>{
 
     return (
       <div className="Home">
-       <h1>전체게시물 LIST</h1>
+       <h1>전체게시물 LIST - back 연결</h1>
         {
           boards.map( (board)=> <BoardItem key={board.id} board={board}/> )
         }   
